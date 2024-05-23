4c8d1b6715007bfaaea5432656ebc09c4d10ad30
busettod <daniel.busetto.db@gmail.com>
2024-05-23 17:16:02 +0200

    frontend modfied

--- src/gitmess-ui/src/lib/payloads/repo.payload.ts
+++ src/gitmess-ui/src/lib/payloads/repo.payload.ts
@@ -2,4 +2,5 @@ import type { GetTreeResponse } from "$lib/models/api.model";
 
 export interface RepoPayload {
 	response: GetTreeResponse;
+	
 }
--- src/gitmess-ui/src/routes/repositories/[repo]/tree/[branch]/[...files]/+page.svelte
+++ src/gitmess-ui/src/routes/repositories/[repo]/tree/[branch]/[...files]/+page.svelte
@@ -2,16 +2,28 @@
 	import { page } from '$app/stores';
 	import type { RepoPayload } from '$lib/payloads/repo.payload';
 	import GitDirectory from '$lib/components/GitDirectory.svelte';
-	import { CodeBlock } from '@skeletonlabs/skeleton';
-
+	import {CodeBlock, ListBox, ListBoxItem} from '@skeletonlabs/skeleton';
+	import {getBranches} from "$lib/api";
+	
 	export let data: RepoPayload;
+
+	const branches = async () => {
+		return await getBranches(
+			$page.params.repo,
+		);
+	};
+	console.log(branches);
 </script>
 
 <div class="card p-4">
 	<header>
 		<h3 class="h3">{$page.params.repo}</h3>
-		<h4 class="h4">{$page.params.branch}</h4>
 	</header>
+	<ListBox>
+		{#each branches as branch}
+			<ListBoxItem value={branch}>{branch}</ListBoxItem>
+		{/each}
+	</ListBox>
 
 	<article class="mt-4">
 		{#if data.response.tree}
